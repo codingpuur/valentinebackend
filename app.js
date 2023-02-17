416c86ec3691d55e4caf47bb61a15c63d404b270
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-17 12:50:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middlewares/error');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,7 +12,10 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({ path: 'backend/config/config.env' });
 }
-
+app.use(cors({
+    origin: 'https://hilarious-paletas-3eb398.netlify.app',
+    allowedHeaders: ['Content-Type']
+  }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
