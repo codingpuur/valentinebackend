11d55ab4589470416fb2d9c1ce48b3aea53ba7ae
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-18 17:45:17 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,9 +7,9 @@ exports.isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
 
     const { token } = req.cookies;
 
-//     if (!token) {
-//         return next(new ErrorHandler("Please Login to Access", 401))
-//     }
+    if (!token) {
+        return next(new ErrorHandler("Please Login to Access", 401))
+    }
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decodedData.id);
