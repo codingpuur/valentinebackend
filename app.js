36cb663e5468519beedfb7cbca29dddbc9d056ed
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-18 22:37:32 +0530

    Update app.js
    cors change remove origin

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ const app = express();
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({ path: 'config/config.env' });
 }
-app.use(cors({
-    origin: 'https://valentinesaga.com/'
-    
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
