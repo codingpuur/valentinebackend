134154c0c9cbe860fda2c7620b72b520be8f4301
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-19 17:17:20 +0530

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,10 +2,10 @@ const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        
+         sameSite: 'none', 
+        secure: true
     }
 
     res.status(statusCode).cookie('token', token, options).json({
