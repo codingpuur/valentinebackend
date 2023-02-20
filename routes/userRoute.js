83e051e7ef86f5c7c8db908eb78201c8b2c9a61f
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-20 07:38:01 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,20 +8,20 @@ router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logoutUser);
 
-router.route('/me').get( getUserDetails);
+router.route('/me').get(isAuthenticatedUser, getUserDetails);
 
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 
-router.route('/password/update').put( updatePassword);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 
-router.route('/me/update').put( updateProfile);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
-router.route("/admin/users").get( authorizeRoles("admin"), getAllUsers);
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
 
 router.route("/admin/user/:id")
-    .get( authorizeRoles("admin"), getSingleUser)
-    .put( authorizeRoles("admin"), updateUserRole)
-    .delete( authorizeRoles("admin"), deleteUser);
+    .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
