a50e7db9eba659ee667b161e4e159be99b78b4ea
shubham chaudhari <70107428+codingpuur@users.noreply.github.com>
2023-02-20 07:31:17 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -13,15 +13,15 @@ router.route('/me').get( getUserDetails);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 
-router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/password/update').put( updatePassword);
 
-router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/me/update').put( updateProfile);
 
-router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router.route("/admin/users").get( authorizeRoles("admin"), getAllUsers);
 
 router.route("/admin/user/:id")
-    .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser)
-    .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole)
-    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
+    .get( authorizeRoles("admin"), getSingleUser)
+    .put( authorizeRoles("admin"), updateUserRole)
+    .delete( authorizeRoles("admin"), deleteUser);
 
 module.exports = router;
